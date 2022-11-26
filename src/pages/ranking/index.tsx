9904d52712ed7c0eeaeaba6d2ab128f366d1b070
Samuel Heinzelmann <samukahweb@gmail.com>
2022-11-25 22:00:05 -0300

    Ranking

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,25 +1,64 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
+import axios from 'axios'
+import { getImageSize } from 'next/dist/server/image-optimizer';
 
+import guesses from '../../assets/guesses.json'
+
+function groupBy(array: any, key: any) {
+    return array.reduce((acc: any, item: any) => {
+
+        const group = item[key][0] ? item[key][0].Description : 'Sem Grupo'
+
+        if (!acc[group]) acc[group] = []
+        acc[group].push(item)
+        return acc
+    }, [])
+}
 
 
 const Ranking: NextPage = () => {
 
+    const [games, setGames] = useState<any>(null)
+    const [user, setUser] = useState<any>(null)
+
+    useEffect(()=>{
+
+        (async function () {
+            const games = await axios.get('https://api.fifa.com/api/v3/calendar/matches?language=pt&idCompetition=17&IdSeason=255711')
+            setGames(games.data)
+        })()
+
+    }, [])
+
+
+let ranking = guesses.reduce((group, gs) => {
+  let newkey = gs['user']
+  if(!group[newkey]){
+    group[newkey]=[]
+  }
+
+//const game = games.Results.filter((g: any) => {
+  //    return g.Home && g.Home.Abbreviation == gs.HomeISO && g.Away.Abbreviation && g.Away.Abbreviation == gs.AwayISO
+  // })
+
+  group[newkey].push({user:gs.user, points: 1})
+  return group
+}, [])
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-900 to-gray-900 ">
-            <div className="flex flex-col md:flex-row items-center gap-6">
-
-                <div className="flex flex-col bg-white shadow rounded p-4 gap-2">
-
-
-                </div>
-
+        <div className="flex flex-col text-white items-center justify-center bg-gradient-to-r from-green-900 to-gray-900 ">
+            <h1>{games && games.Results[0].CompetitionName[0].Description}</h1>
+            <div className="flex justify-center w-screen md:p-10 flex-wrap gap-4 text-black">
+                {ranking && ranking.map((u: any) => {
+                    //@ts-ignore
+                    return <>{u.points}</>
+                })}
             </div>
-
+            {/* {games && console.log(groupBy(games.Results, 'GroupName'))} */}
         </div>
     )
 }
